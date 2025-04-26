55524e0bc88b9d157f2066005fec892b35c29073
ahmetyusufbirdir03 <168556979+ahmetyusufbirdir03@users.noreply.github.com>
2025-04-26 14:27:19 +0300

    Update App.jsx

--- quiz_platform/src/App.jsx
+++ quiz_platform/src/App.jsx
@@ -1,20 +1,20 @@
-import './App.css'
+import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignIn from "./auth/signin";
-import Login from "./auth/login"; 
+import Login from "./auth/login";
 import EmailVerification from "./auth/emailVerification";
 import Terms from "./auth/terms";
 import CreateProfile from "./auth/createProfile";
-import Home from './pages/Home';
+import Home from "./pages/Home";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/signin" element={<SignIn />} />      
+        <Route path="/signin" element={<SignIn />} />
         <Route path="/forgetPassword" element={<EmailVerification />} />
-        <Route path="/terms" element={<Terms />} />                                
+        <Route path="/terms" element={<Terms />} />
         <Route path="/create_profile" element={<CreateProfile />} />
 
         <Route path="/home" element={<Home />} />
